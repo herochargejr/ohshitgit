29ba1b9323b8acb71cd5490af02e71a29ba87df5
herocharge <techwithbhargav@gmail.com>
2022-09-23 12:54:35 +0530

    added my secret hehe

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import *
 
 app = Flask(__name__)
 
+@app.route('/secret_route')
+def secret():
+    password = request.args.get('secret_key')
+    if password == 'ihatemybosshealwaysmakesmedomoreworkbruhalsofollowhackingcluboninsta':
+        return "Your secret is 0xCTF{ca75_4R3_c001_Bu7_L34Rn_g17_8Ruh}"
+    else:
+        return "booo you can't find my secret dum dum"
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-# maybe i should add a feature which only I can see
-# it'll be soo fun
+ 
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000)
